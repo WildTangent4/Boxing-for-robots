c979b5408353d17736c946a566d90a2d59125a3c
WildTangent4 <wildtangent4@gmail.com>
2024-08-02 14:32:44 +0100

    added explanatory comments to physics manager declarations

--- drone-game/headers/PhysicsManager.h
+++ drone-game/headers/PhysicsManager.h
@@ -10,12 +10,29 @@ class PhysicsManager {
 	std::atomic<bool> gameIsRunning = true;
 	std::vector<GameObject> Objects;
 
+	/// <summary>
+	/// runs physics tick in a loop
+	/// </summary>
+	/// TODO: should account for physics tick length
 	void physicsLoop();
+	/// <summary>
+	/// runs a physics tick for each object
+	/// </summary>
 	void runTickForAllObjects();
+	/// <summary>
+	/// adds forces to velocity, and velocity to position
+	/// </summary>
+	/// <param name="obj"></param>
 	void applyTick(GameObject obj);
 public:
 	PhysicsManager();
+	/// <summary>
+	/// begin physics simulation
+	/// </summary>
 	void simulate();
+	/// <summary>
+	/// stop hysicis simulation NOTE: currently deletes all objects
+	/// </summary>
 	void endSimulation();
 	~PhysicsManager();
 	PhysicsManager(const PhysicsManager&) = delete;
