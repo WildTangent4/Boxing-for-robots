0c4556c6ebd094842c773eec08236b28b13ffaa8
WildTangent4 <wildtangent4@gmail.com>
2024-09-28 19:13:33 +0100

    states now update correctly even while not stunned

--- drone-game/source/Enemy.cpp
+++ drone-game/source/Enemy.cpp
@@ -24,15 +24,18 @@ void Enemy::Damage(int amount)
 
 void Enemy::RunAI(Behaviour type,GameObject* player)
 {
-	if (currentState == STUNNED) {
-		this->stunTimeRemaining = this->stunTimeRemaining - GetFrameTime();
-		if (this->stunTimeRemaining <= 0) {
-			adjustAggression();
-			this->currentState = findNextAction(type,player);
-			
-			this->texture = this->sprites.rest;
+	this->stunTimeRemaining = this->stunTimeRemaining - GetFrameTime();
+	printf("\n%f : remaningin stun time", this->stunTimeRemaining);
+	if (this->stunTimeRemaining <= 0) {
+		if (this->currentState == State::STUNNED) {
+			this->currentState = State::WAIT;
 		}
+		adjustAggression();
+		this->currentState = findNextAction(type,player);
+			
+		this->texture = this->sprites.rest;
 	}
+	
 }
 
 void Enemy::adjustAggression()
@@ -40,7 +43,6 @@ void Enemy::adjustAggression()
 	//include features like if player is moving towards enemy (i.e. looks like they might punch or has block lowered)
 	this->aggressionLevel = this->aggressionLevel +
 		((this->baseAggressionIncreaseRate)*GetFrameTime());
-	printf("%f", this->aggressionLevel);
 }
 
 void Enemy::applyState(GameObject* target)
@@ -55,6 +57,20 @@ void Enemy::applyState(GameObject* target)
 		//todo make hit function for player - probably worth creating a class "damagable" that enemy and player inherit from
 	case State::PUSH:
 		this->texture = this->sprites.moveAnim.at(0);//todo create play animation function
+		//move towards player
+		this->pos =
+			Vector3Add(
+				(Vector3Scale(
+					Vector3Scale(
+						Vector3Normalize(
+							Vector3Subtract(this->pos, target->pos)
+						),
+						this->moveSpeed
+					),
+					GetFrameTime())),
+				this->pos
+			);
+
 	case State::WAIT:
 		this->texture = this->sprites.rest;
 	case State::STUNNED:
@@ -71,31 +87,38 @@ Enemy::State Enemy::findNextAction(Behaviour type, GameObject* player)
 	Vector3 playerPos = player->pos;
 
 	if (this->currentState == State::STUNNED) {
+		printf("state is STUNNED");
 		return State::STUNNED;
 	}
 
 	//if player is visible but not in fighting range, approach
 	if (Vector3Distance(playerPos, this->pos) < this->aggroDist && Vector3Distance(playerPos, this->pos) > this->attackDist) {
+		printf("state is PUSH");
 		return State::PUSH;
 	}
 	//if player is in fighting range and aggression is high, punch
 	else if (Vector3Distance(playerPos, this->pos) < this->aggroDist && this->aggressionLevel > 0.8) {
+		printf("state is PUNCH");
 		return State::PUNCH;
 	}
 	//if the enemy is about to punch, lower block to give player time to react
 	else if (Vector3Distance(playerPos, this->pos) < this->aggroDist && this->aggressionLevel > 0.6) {
+		printf("state is WAIT");
 		return State::WAIT;
 	}
 	//if player is in fighting range and aggression is low, block
 	else if ((Vector3Distance(playerPos, this->pos) < this->aggroDist && this->aggressionLevel > 0.2)) {
+		printf("state is BLOCK");
 		return State::BLOCK;
 	}
 	//if player is in fighting range and aggression is very low, retreat
 	else if (Vector3Distance(playerPos, this->pos) < this->aggroDist) {
+		printf("state is RETREAT");
 		return State::RETREAT;
 	}
 	//if player is out of aggro range and do nothing
 	else {
+		printf("state is WAIT");
 		return State::WAIT;
 	}
 }
