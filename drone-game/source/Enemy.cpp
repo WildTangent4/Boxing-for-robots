5d1be322de2d3edba22d20d90fad87e49c5aa0a9
WildTangent4 <98473126+WildTangent4@users.noreply.github.com>
2024-10-25 14:23:55 +0100

    removes debug print statements

--- drone-game/source/Enemy.cpp
+++ drone-game/source/Enemy.cpp
@@ -202,19 +202,15 @@ Enemy::State Enemy::getReactionToPlayerActions(Player* player)
 	// NOTE: if statement is ordered by priority
 
 	if (playerIsPunching) {
-		printf("enemy AI sees that player is punching");
 		return this->playerIsPunchingResponse.at(GetRandomValue(0, this->playerIsPunchingResponse.size() - 1));
 	}
 	else if (playerIsLowPosture) {
-		printf("enemy AI sees that player is low posture");
 		return this->playerIsLowPostureResponse.at(GetRandomValue(0, this->playerIsLowPostureResponse.size() - 1));
 	}
 	else if (playerIsBlocking) {
-		printf("enemy AI sees that player is blocking");
 		return this->playerIsBlockingResponse.at(GetRandomValue(0, this->playerIsBlockingResponse.size() - 1));
 	}
 	else if (playerIsApproaching) {
-		printf("enemy AI sees that player is approaching");
 		return this->playerIsApproachingResponse.at(GetRandomValue(0, this->playerIsApproachingResponse.size() - 1));
 	}
 	else {
